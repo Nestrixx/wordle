db210c0baa77f71ce296671101cc6e8402fb2175
Christian McKinney <christian.mckinney@fdmgroup.com>
2024-04-30 12:14:51 -0500

    added back in all of the input boxes and added optional chaining to prevent accessing undefined values.

--- src/code/App.tsx
+++ src/code/App.tsx
@@ -5,18 +5,22 @@ import "../styles/app.scss";
 import { useState } from "react";
 
 const App = () => {
-const [gameState, setGameState] = useState<string[][]>([[]]);
-const [gameRound, setGameRound] = useState<number>(0);
+  const [gameState, setGameState] = useState<string[][]>([[]]);
+  const [gameRound, setGameRound] = useState<number>(0);
 
   return (
     <div className="app">
       <Navbar />
       <div className="gameWrapper">
-          <InputBoxes gameState={gameState} />
-          <Keyboard gameState={gameState} setGameState={ setGameState} gameRound={gameRound}/>
+        <InputBoxes gameState={gameState} />
+        <Keyboard
+          gameState={gameState}
+          setGameState={setGameState}
+          gameRound={gameRound}
+        />
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/code/InputBoxes.tsx
+++ src/code/InputBoxes.tsx
@@ -5,47 +5,44 @@ type Props = {
 }
 
 const InputBoxes: React.FC<Props> = ({gameState}) => {
-  // const columns = 5;
-  // const inputsPerColumn = 5;
-  //refactor this to use loops to generate the rows.
 
   return (
     <div className="boxWrapper">
       <div className="column1">
-        <input className="letterBox" type="text" maxLength={1} value={gameState[0][0] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={gameState[0][1] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={gameState[0][2] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={gameState[0][3] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={gameState[0][4] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[0]?.[0] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[0]?.[1] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[0]?.[2] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[0]?.[3] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[0]?.[4] || ''} readOnly />
       </div>
-      {/* <div className="column2">
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[0] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[1] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[2] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[3] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[4] || ''} readOnly />
+      <div className="column2">
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[1]?.[0] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[1]?.[1] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[1]?.[2] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[1]?.[3] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[1]?.[4] || ''} readOnly />
       </div>
       <div className="column3">
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[0] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[1] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[2] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[3] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[4] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[2]?.[0] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[2]?.[1] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[2]?.[2] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[2]?.[3] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[2]?.[4] || ''} readOnly />
       </div>
       <div className="column4">
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[0] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[1] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[2] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[3] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[4] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[3]?.[0] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[3]?.[1] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[3]?.[2] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[3]?.[3] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[3]?.[4] || ''} readOnly />
       </div>
       <div className="column5">
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[0] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[1] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[2] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[3] || ''} readOnly />
-        <input className="letterBox" type="text" maxLength={1} value={currentGuess[4] || ''} readOnly />
-      </div> */}
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[4]?.[0] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[4]?.[1] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[4]?.[2] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[4]?.[3] || ''} readOnly />
+        <input className="letterBox" type="text" maxLength={1} value={ gameState[4]?.[4] || ''} readOnly />
+      </div>
     </div>
   );
 };
